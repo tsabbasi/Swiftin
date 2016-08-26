f87b909757aac34feaa633a5f92ab7e06da4499d
Tuba Abbasi <tsabbasi@gmail.com>
2016-08-26 19:14:50 -0400

    Vending Machine App

---
+++ Apps/VendingMachine/VendingMachineStarterFiles/VendingMachine/VendingMachine.swift
@@ -0,0 +1,167 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Tuba Abbasi on 8/26/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// Protocols
+
+protocol VendingMachineType {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: ItemType] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: ItemType])
+    func vend(selection: VendingSelection, quantity: Double) throws
+    func deposit(amount: Double)
+}
+
+
+protocol ItemType {
+    var price: Double { get }
+    var quantitiy: Double { get set }
+}
+
+
+// Error Types
+
+enum InventoryError: ErrorType {
+    case InvalidResource
+    case ConversionError
+    case InvalidKey
+}
+
+
+enum VendingMachineError: ErrorType {
+    case InvalidSelection
+    case OutOfStock
+    case InsufficientFunds(required: Double)
+    
+}
+
+// Helper Classes
+
+class PlistConverter {
+    class func dictionaryFromFile(resource: String, ofType type: String) throws -> [String: AnyObject] {
+        
+        guard let path = NSBundle.mainBundle().pathForResource(resource, ofType: type) else {
+            throw InventoryError.InvalidResource
+        }
+        
+        guard let dictionary = NSDictionary(contentsOfFile: path), let castDictionary = dictionary as? [String: AnyObject] else {
+            throw InventoryError.ConversionError
+        }
+        
+        return castDictionary
+    }
+}
+
+
+class InventoryUnarchiver {
+    class func vendingInventoryFromDictionary(dictionary: [String: AnyObject]) throws -> [VendingSelection: ItemType] {
+        
+        var inventory: [VendingSelection: ItemType] = [:]
+        
+        for (key, value) in dictionary {
+            if let itemDict = value as? [String: Double], let price = itemDict["price"], let quantity = itemDict["quantity"] {
+                
+                let item = VendingItem(price: price, quantitiy: quantity)
+                
+                
+                guard let key = VendingSelection(rawValue: key) else {
+                    throw InventoryError.InvalidKey
+                }
+                
+                inventory.updateValue(item, forKey: key)
+            }
+        }
+        
+        return inventory
+    }
+}
+
+
+// Concrete Types
+
+enum VendingSelection: String {
+    case Soda
+    case DietSoda
+    case Chips
+    case Cookie
+    case Sandwich
+    case Wrap
+    case CandyBar
+    case PopTart
+    case Water
+    case FruitJuice
+    case SportsDrink
+    case Gum
+    
+    func icon() -> UIImage {
+        if let image = UIImage(named: self.rawValue) {
+            return image
+        } else {
+            return UIImage(named: "Default")!
+        }
+    }
+}
+
+
+struct VendingItem: ItemType {
+    let price: Double
+    var quantitiy: Double
+}
+
+
+class VendingMachine: VendingMachineType {
+    let selection: [VendingSelection] = [.Soda, .DietSoda, .Chips, .Cookie, .Sandwich, .Wrap, .CandyBar, .PopTart, .Water, .FruitJuice, .SportsDrink, .Gum]
+    var inventory: [VendingSelection : ItemType]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : ItemType]) {
+        self.inventory = inventory
+    }
+    
+    func vend(selection: VendingSelection, quantity: Double) throws {
+        
+        // is valid selection
+        guard var item = inventory[selection] else {
+            throw VendingMachineError.InvalidSelection
+        }
+        
+        // is in stock
+        
+        guard item.quantitiy > 0 else {
+            throw VendingMachineError.OutOfStock
+        }
+        
+        item.quantitiy -= quantity
+        inventory.updateValue(item, forKey: selection)
+        
+        let totalPrice = item.price * quantity
+        
+        // have enough funds
+        
+        if amountDeposited >= totalPrice {
+            amountDeposited -= totalPrice
+            
+        } else {
+            
+            let amountRequired = totalPrice - amountDeposited
+            VendingMachineError.InsufficientFunds(required: amountRequired)
+        }
+    }
+    
+    
+    func deposit(amount: Double) {
+        // add code
+    }
+    
+}
+
